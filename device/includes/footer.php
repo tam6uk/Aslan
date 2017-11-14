e37855ba7dcaf2f2a2a7ea57b21116ae9605a406
Berzeg07 <berezg86@gmail.com>
2017-11-14 11:09:18 +0300

    Разметка главной Device
    Завершил разметку главной страницы

--- device/includes/footer.php
+++
@@ -1,7 +0,0 @@
-
-
-<!-- JS_BLOCK -->
-<script src="libs/jquery/jquery-1.11.1.min.js"></script>
-<script src="libs/owl.carousel/owl.carousel.js"></script>
-<script src="js/fonts.js"></script>
-<script src="js/common.js"></script>
--- device/includes/links.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/fonts.css">
